a8cf067fb38e800978aaf8fb625503fb7d92d4fc
Gregoire Dechaine <gregdechaine@me.com>
2022-07-05 23:20:23 -0400

    Build Config from example

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,34 +1,43 @@
 import { defineUserConfig } from 'vuepress'
+import { defaultTheme } from '@vuepress/theme-default'
+import { head, navbarEn } from './configs'
 
 export default defineUserConfig({
-  lang: 'en-US',
+  // set site base to default value
+  base: '/',
+
+  // extra tags in `<head>`
+  head,
+
+  // page title
   title: 'Bimlify',
+
+  // description
   description: 'Bimlify - Simple Process -> Continous Design',
-  themeConfig: {
-    repo: '',
-    editLinks: true,
-    docsDir: '',
+
+  // configure default theme
+  theme: defaultTheme({
     editLinkText: 'Edit',
-    lastUpdated: false,
-    logo: '/assets/img/bimlify_logo.png',
-    nav: [
-      {
-        text: 'Guide',
-        link: '/guide/',
-      },
-      {
-        text: 'Revit Config',
-        link: '/revit_config/'
-      },
-      {
-        text: 'Autodesk Docs',
-        link: 'https://acc.autodesk.com'
-      },
-      {
-        text: 'Knowledge Network',
-        link: 'https://knowledge.autodesk.com'
+    // logo: '/images/hero.png',
+    repo: 'gregoiredechaine/vuepress-next',
+    docsDir: 'docs',
+
+    // theme-level locales config
+    locales: {
+      /**
+       * English locale config
+       *
+       * As the default locale of @vuepress/theme-default is English,
+       * we don't need to set all of the locale fields
+       */
+      '/': {
+        // navbar
+        navbar: navbarEn,
+        // sidebar
+        sidebar: sidebarEn,
+        // page meta
+        editLinkText: 'Edit',
       }
-    ],
-    sidebar: 'auto',
-  },
+    }
+  })
 })
